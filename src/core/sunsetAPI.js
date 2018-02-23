0aab87dc16f3f49d82bf36c52709879b5325a8be
Shakti <srajpurohit@zendesk.com>
2018-02-22 19:42:30 -0800

    check env

--- src/core/sunsetAPI.js
+++ src/core/sunsetAPI.js
@@ -1,7 +1,7 @@
 import SunsetWx from 'node-sunsetwx';
 import keys from '../config/keys';
 
-console.log('keys', keys, process.env)
+console.log('keys', keys, process)
 const sunsetwx = new SunsetWx(keys);
 
 export function sunsetLogin() {
